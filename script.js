503686f08d7d8d4a2f390e414b63f9295d12db34
Tania Torres <20519161+myfriendtania@users.noreply.github.com>
2022-01-25 04:45:09 +0000

    wip; concluded lesson

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 const URL = "https://api.openweathermap.org/data/2.5/weather?q=Dallas&appid=88723180e3c19e3235dfaf41df75b619&units=imperial"
 
 const sectionElement = document.querySelector('section');
+
+fetch(URL)
+.then(response => response.json())
+  .then(data => {
+    sectionElement.innerHTML = `
+    <h2>Current Weather for Dallas, TX</h2>
+    <p>75201</p>
+    <p>Current Temperature: ${data.main.temp}</p>
+    <p>Current condition: It's raining, bitch! Stay home!</p>
+    <p> How's the sky looking? ${data.weather[0].description}</p>
+     <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png"/>
+     `
+  })
+ 
