8d4108cb8774855bacf25bd3baea64797cb0686b
Tania Torres <20519161+myfriendtania@users.noreply.github.com>
2022-01-25 02:47:49 +0000

    wip; added jvscript to this project, also a gif

---
+++ script.js
@@ -0,0 +1,3 @@
+const URL = "https://api.openweathermap.org/data/2.5/weather?q=Dallas&appid=88723180e3c19e3235dfaf41df75b619&units=imperial"
+
+const sectionElement = document.querySelector('section');
